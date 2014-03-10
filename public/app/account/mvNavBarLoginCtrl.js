d1f4284092b4ee75400160efccff7c0067dbd4e5
cavila <cavila@IT-DSK-597.gap.com>
2014-03-10 15:58:55 -0600

    add: signin and singout added

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,5 +1,5 @@
 angular.module('app')
-	.controller('mvNavBarLoginCtrl', function ($scope, $http, mvIdentity, mvNotifier, mvAuth) {
+	.controller('mvNavBarLoginCtrl', function ($scope, $http, mvIdentity, mvNotifier, mvAuth, $location) {
 		$scope.identity = mvIdentity;
 		$scope.signin = function (username, password) {
 			mvAuth.authenticateUser(username, password).then( function(success) {
@@ -9,5 +9,13 @@ angular.module('app')
 					mvNotifier.notify('Username/Password combination incorrect.');
 				}
 			})
-		}
+		};
+		$scope.signout = function () {
+			mvAuth.logoutUser().then(function () {
+				$scope.username = "";
+				$scope.password = "";
+				mvNotifier.notify('You have successfully been signed out.');
+				$location.path('/');
+			})
+		};
 	});
--- server/config/routes.js
+++ server/config/routes.js
@@ -11,4 +11,9 @@ module.exports = function (app) {
 	});
 
 	app.post('/login', auth.authenticate);
+
+	app.post('/logout', function (req, res) {
+		req.logout();
+		res.end();
+	});
 };
